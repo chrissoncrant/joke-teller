5f5962393675b3370af144153e664b5855d60031
Chris Soncrant <christopher.soncrant@gmail.com>
2022-05-26 13:17:12 -0700

    setup Joke functionality with the text to speech SDK

--- script.js
+++ script.js
@@ -1,55 +1,43 @@
 import {VOICERSS_API_KEY, RAPID_API_VOICERSS_KEY, JOKE_API_KEY} from './config.js';
-
-const audio = document.getElementById('audio');
 const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// VoiceRSS Javascript SDK
+const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+
+async function setAudioText(text) {
+    VoiceRSS.speech({
+        key: VOICERSS_API_KEY,
+        src: text,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+};
+
+// test('hello');
 
 async function getJoke() {
-    const options = {
-        method: 'GET',
-        url: 'https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes',
-        headers: {
-          'X-RapidAPI-Host': 'jokes-by-api-ninjas.p.rapidapi.com',
-          'X-RapidAPI-Key': JOKE_API_KEY
-        }
-      };
-      const res = await axios.request(options);
-    //   console.log(res.data[0].joke);
-      return res.data[0].joke;
-}
-
-async function speak({trans, lang, voice, speed}) {
-    const options = {
-        method: 'GET', 
-        url: `https://voicerss-text-to-speech.p.rapidapi.com/`,
-        params: {
-            key: VOICERSS_API_KEY,
-            src: trans || 'text is not defined',
-            hl: lang,
-            v: voice,
-            r: speed,
-            c: 'MP3',
-            f: '8khz_8bit_stereo',
-            b64: true,
-        },
-        headers: {
-            'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com',
-            'X-RapidAPI-Key': RAPID_API_VOICERSS_KEY
-            }
-    };
-    const res = await axios.request(options);
-    audio.setAttribute('src', res.data);
+    try {
+        let response = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit');
+        response = await response.json();
+        if (response.type === 'single') {
+            console.log('single', response)
+            return response.joke;
+        } else {
+            return `${response.setup} ... \n ${response.delivery}`
+        };
+    }
+    catch(err) {
+        console.log('Error getting joke', err);
+    }
 }
 
 button.addEventListener('click', async () => {
-    const speechInput = {
-        trans: await getJoke(),
-        lang: 'en-gb',
-        voice: 'Lily',
-        //speed range: -10 to 10;
-        speed: 1
-    }
-    await speak(speechInput);
-    audio.play();
-})
-
-
+    const joke = await getJoke();
+    setAudioText(joke);
+    audioElement.play(); 
+}) 
